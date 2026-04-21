156e348981313063a4de1991aaf17b104b9e4f0e
Eirk <paulsonerik0@gmail.com>
2026-04-20 22:57:01 -0400

    -moved files -updated west.yml -using logging -builds and runs on esp32s3 featherboard

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main);
+
+int main(void)
+{
+	LOG_INF("Hello World! %s", CONFIG_BOARD_TARGET);
+
+	return 0;
+}
--- uc-app/src/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
-	return 0;
-}
