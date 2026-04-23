817d9885df0647cd467985322cbe5cc5b408d845
Erik <paulsonerik0@gmail.com>
2026-04-23 03:01:35 -0400

    - LED strip sample brought into main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,67 @@
-#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 #include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/led_strip.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/util.h>
+
+#define STRIP_NODE		DT_ALIAS(led_strip)
+
+#if DT_NODE_HAS_PROP(DT_ALIAS(led_strip), chain_length)
+#define STRIP_NUM_PIXELS	DT_PROP(DT_ALIAS(led_strip), chain_length)
+#else
+#error Unable to determine length of LED strip
+#endif
+
+#define DELAY_TIME K_MSEC(500)
+
+#define RGB(_r, _g, _b) { .r = (_r), .g = (_g), .b = (_b) }
+
+static const struct led_rgb colors[] = {
+	RGB(0xf0, 0x00, 0x00), /* red */
+	RGB(0x00, 0xf0, 0x00), /* green */
+	RGB(0x00, 0x00, 0xf0), /* blue */
+};
+
+static struct led_rgb pixels[STRIP_NUM_PIXELS];
+
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
 	LOG_INF("Hello World! %s", CONFIG_BOARD_TARGET);
+
+	size_t color = 0;
+	int rc;
+
+	if (device_is_ready(strip)) {
+		LOG_INF("Found LED strip device %s", strip->name);
+	} else {
+		LOG_ERR("LED strip device %s is not ready", strip->name);
+		return 0;
+	}
+
+	LOG_INF("Displaying pattern on strip");
+	while (1) {
+		for (size_t cursor = 0; cursor < ARRAY_SIZE(pixels); cursor++) {
+			memset(&pixels, 0x00, sizeof(pixels));
+			memcpy(&pixels[cursor], &colors[color], sizeof(struct led_rgb));
+
+			rc = led_strip_update_rgb(strip, pixels, STRIP_NUM_PIXELS);
+			if (rc) {
+				LOG_ERR("couldn't update strip: %d", rc);
+			}
+
+			k_sleep(DELAY_TIME);
+		}
+
+		color = (color + 1) % ARRAY_SIZE(colors);
+	}
 
 	return 0;
 }
